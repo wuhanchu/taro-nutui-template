3dbab3c0ec1f3f887a0e0caa9d06340a3897f8d5
wuhanchu <whcwuhanchu@gmail.com>
2023-12-04 21:50:03 +0800

    first commit

---
+++ config/index.ts
@@ -0,0 +1,88 @@
+const config = {
+  projectName: "taro-nutui-template",
+  date: "2023-12-4",
+  designWidth(input) {
+    // 配置 NutUI 375 尺寸
+    if (input?.file?.replace(/\\+/g, "/").indexOf("@nutui") > -1) {
+      return 375;
+    }
+    // 全局使用 Taro 默认的 750 尺寸
+    return 750;
+  },
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2,
+    375: 2 / 1,
+  },
+  cache: {
+    enable: true,
+  },
+  sourceRoot: "src",
+  outputRoot: "dist",
+  plugins: ["@tarojs/plugin-html"],
+  defineConstants: {},
+  copy: {
+    patterns: [],
+    options: {},
+  },
+  framework: "react",
+  compiler: {
+    type: "webpack5",
+    prebundle: { enable: false },
+  },
+  mini: {
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {
+          selectorBlackList: ["nut-"],
+        },
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 1024, // 设定转换尺寸上限
+        },
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: "module", // 转换模式，取值为 global/module
+          generateScopedName: "[name]__[local]___[hash:base64:5]",
+        },
+      },
+    },
+  },
+  h5: {
+    publicPath: "/",
+    staticDirectory: "static",
+    // esnextModules: ['nutui-react'],
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {
+          selectorBlackList: ["nut-"],
+        },
+      },
+      autoprefixer: {
+        enable: true,
+        config: {},
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: "module", // 转换模式，取值为 global/module
+          generateScopedName: "[name]__[local]___[hash:base64:5]",
+        },
+      },
+    },
+  },
+};
+
+module.exports = function (merge) {
+  if (process.env.NODE_ENV === "development") {
+    return merge({}, config, require("./dev"));
+  }
+  return merge({}, config, require("./prod"));
+};
